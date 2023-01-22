cde8eecef6352b9cff49abf1ca4b7f9240f2e2a9
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-01-22 14:23:58 +0530

    created navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Contact from "./Pages/Contact";
+import HomePage from "./Pages/HomePage";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Navbar />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-around text-white font-mono">
+      {/* creating the name logo */}
+      <div
+        className="h-[55px] w-[45px] bg-[#64ffda] flex items-center justify-center my-4"
+        style={{
+          clipPath:
+            "polygon(50% 0, 0 30%, 0 70%, 50% 100%, 100% 70%, 100% 30%)",
+        }}
+      >
+        <div
+          className="h-[50px] w-[42px] flex items-center justify-center text-2xl"
+          style={{
+            clipPath:
+              "polygon(50% 0, 0 30%, 0 70%, 50% 100%, 100% 70%, 100% 30%)",
+          }}
+        >
+          <a className="font-bold" href="#homepage">V</a>
+        </div>
+      </div>
+
+      {/* creating the navigation menu */}
+      <ul className="flex space-x-8 text-gray-200">
+        <li>
+          <a href="#homepage">
+            <span className="text-[#64ffda] mr-1 text-sm">01.</span> Home
+          </a>
+        </li>
+
+        <li>
+          <a href="#education">
+            <span className="text-[#64ffda] mr-1 text-sm">02.</span> Education
+          </a>
+        </li>
+
+        <li>
+          <a href="#skills">
+            <span className="text-[#64ffda] mr-1 text-sm">03.</span> Skills
+          </a>
+        </li>
+
+        <li>
+          <a href="#projects">
+            <span className="text-[#64ffda] mr-1 text-sm">04.</span> Projects
+          </a>
+        </li>
+
+        <li>
+          <a href="#contact">
+            <span className="text-[#64ffda] mr-1 text-sm">05.</span> Contact
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
